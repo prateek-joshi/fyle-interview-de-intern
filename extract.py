84d97533b8ce4f973a584e7573f749e5a5297117
Prateek Joshi <pjoshi22900@gmail.com>
2021-08-30 16:24:43 +0530

    Add function to load json data

--- extract.py
+++ extract.py
@@ -1,7 +1,27 @@
 # Your imports go here
 import logging
+import os
+import json
 
 logger = logging.getLogger(__name__)
+
+
+def get_data_from_json(dirpath: str) -> dict:
+    
+    # get path of json
+    if 'ocr.json' not in os.listdir(dirpath):
+        print('OCR JSON not found.')
+        return None
+
+    logger.info('get_data_from_json called for file %s', dirpath)
+
+    ocr_json_path = os.path.join(dirpath,'ocr.json')
+    # load json into dictionary
+    with open(ocr_json_path, 'r') as f:
+        data = json.load(f)
+
+    return data
+  
 
 '''
     Given a directory with receipt file and OCR output, this function should extract the amount
@@ -13,9 +33,17 @@ logger = logging.getLogger(__name__)
     float: returns the extracted amount
 
 '''
+
 def extract_amount(dirpath: str) -> float:
 
     logger.info('extract_amount called for dir %s', dirpath)
-    # your logic goes here
+    
+    data = get_data_from_json(dirpath)
+    print(data['Blocks'][1]['Text'])
+    # extract the lines from dictionary
+    # write logic using regex to get all lines with $ or Total or Credit or Debit
 
     return 0.0
+
+if __name__=='__main__':
+    extract_amount('D:\\Projects\\Fyle-Internship-Challenege\\fyle-interview-de-intern\\data\\receipt1')
