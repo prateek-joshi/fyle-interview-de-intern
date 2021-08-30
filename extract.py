75ff348a79d3165fb70f426cc382c9e66cdf1d7c
Prateek Joshi <pjoshi22900@gmail.com>
2021-08-31 01:03:56 +0530

    Update extract_amount

--- extract.py
+++ extract.py
@@ -2,6 +2,7 @@
 import logging
 import os
 import json
+import re
 
 logger = logging.getLogger(__name__)
 
@@ -21,11 +22,11 @@ def get_data_from_json(dirpath: str) -> dict:
 
     logger.info('get_data_from_json called for file %s', ocr_json_path)
     # load json into dictionary
-    with open(ocr_json_path, 'r') as f:
+    with open(ocr_json_path, 'r', encoding='utf-8') as f:
         data = json.load(f)
 
     return data
-  
+
 
 def get_lines_from_dict(data) -> list:
     '''
@@ -45,37 +46,54 @@ def get_lines_from_dict(data) -> list:
     # Iterate over all LINE blocks only and append text line
     for block in blocks[1:]:
         if block['BlockType']=='LINE':
-            lines.append(block['Text'])
+            lines.append(block['Text'].lower())
         else:
             break
 
     return lines
 
 
-'''
-    Given a directory with receipt file and OCR output, this function should extract the amount
-
-    Parameters:
-    dirpath (str): directory path containing receipt and ocr output
-
-    Returns:
-    float: returns the extracted amount
-
-'''
 def extract_amount(dirpath: str) -> float:
+    '''
+        Given a directory with receipt file and OCR output, this function should extract the amount
 
+        Parameters:
+        dirpath (str): directory path containing receipt and ocr output
+
+        Returns:
+        float: returns the extracted amount
+
+    '''
     logger.info('extract_amount called for dir %s', dirpath)
-    
+
     # load json data into a dictionary
     data = get_data_from_json(dirpath)
-    
+
     # extract the lines from dictionary
     lines = get_lines_from_dict(data)
-    print(lines)
 
-    # write logic using regex to get all lines with $ or Total or Credit or Debit
+    lines_with_amount = []
+    for i in range(1,len(lines)-1):
+        # if line contains $
+        if re.search('$', lines[i]):
+            # line contains amount and $ together
+            if re.search(r'[0-9]+\.[0-9]+', lines[i]):
+                lines_with_amount.append(lines[i])
+            # if next line contains the amount
+            elif re.search(r'[0-9]+\.[0-9]+', lines[i+1]):
+                lines_with_amount.append(lines[i+1])
 
-    return 0.0
+        elif any(ele in lines[i] for ele in ['Payment', 'Total', 'Credit', 'Debit']):
+            lines_with_amount.append(lines[i+1])
 
-if __name__=='__main__':
-    extract_amount('D:\\Projects\\Fyle-Internship-Challenege\\fyle-interview-de-intern\\data\\receipt2')
+    # print(lines_with_amount)
+    amounts = []
+    for amt in lines_with_amount:
+        # get only the amount from all the lines
+        a = re.search(r'([0-9]|\.|,)+',amt).group()
+        # remove all ocurrences of commas
+        amounts.append(float(a.replace(',','')))
+
+    # print(amounts)
+
+    return max(amounts)
